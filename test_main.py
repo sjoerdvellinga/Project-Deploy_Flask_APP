ed61dd1aff2427b10c3262d4fa846ae75ce990e7
sjoerdvellinga <github.token>
2023-08-30 13:33:22 -0400

    Import app into test

--- test_main.py
+++ test_main.py
@@ -6,7 +6,12 @@ import json
 import pytest
 import unittest
 import main
+
+from flask import Flask
+import pytest
 from main import APP
+
+app = Flask(__name__)
 
 SECRET = 'TestSecret'
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
