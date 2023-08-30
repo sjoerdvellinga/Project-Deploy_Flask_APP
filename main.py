6fdfc76e928cbc500bdb5974928a2ebae84d1a78
sjoerdvellinga <github.token>
2023-08-29 22:50:35 -0400

    Remove next endpoint

--- main.py
+++ main.py
@@ -60,18 +60,6 @@ def require_jwt(function):
 def health():
     return jsonify("Still healthy ;-)")
 
-@APP.route('/next', methods=['POST', 'GET'])
-def next():
-    var field = "What is on your mind?"
-    var obj = {}
-    obj[field] = "Looking forward to start with the final project ;-)"
-    var myJSON = JSON.stringify(obj)
-
-
-#    return jsonify({'What is next?': NEXT,
-#            'What is in your mind?': MOOD})
-
-
 @APP.route('/auth', methods=['POST'])
 def auth():
     """
